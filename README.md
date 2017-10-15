8af4fa03a2a7bb84f5242fe67202c1c119ba4cd5
6769 <5pipitk@gmail.com>
2017-10-15 17:19:49 +0800

    upload code,v1.0

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # ApiCC98
-Java wrap for api.cc98.org
+
+Java wrap for [https://api.cc98.org](https://api.cc98.org)
+Use Gradle Build Tools;
+### Implementation
+
+Network interface just copy from website with Retrofit's super convenient annotations.
+And Asyn-event is handled with RxJava and Retrofit-rxjava-adapter;
+
+This repo is designed as a network component of new forum app's design.
+
+### Dependency
+
+```
+dependencies {
+    compile 'io.reactivex:rxjava:1.1.1'
+    compile 'com.squareup.retrofit2:retrofit:2.0.2'
+    compile 'com.squareup.retrofit2:converter-gson:2.0.2'
+    compile 'com.squareup.retrofit2:adapter-rxjava:2.0.2'
+    testCompile group: 'junit', name: 'junit', version: '4.12'
+}
+```
+
---
+++ src/main/java/win/pipi/api/CC98ApiList.java
@@ -0,0 +1,65 @@
+package win.pipi.api;
+
+
+import rx.Observable;
+import rx.Subscriber;
+import win.pipi.api.data.*;
+import win.pipi.api.network.CC98APIInterface;
+import win.pipi.api.network.CC98APIManager;
+
+import java.util.ArrayList;
+
+public class CC98ApiList {
+    public static void main(String[] args){
+
+
+        CC98ApiList apiList=new CC98ApiList();
+        apiList.request();
+    }
+    public void request() {
+
+
+
+        // 步骤5:创建 网络请求接口 的实例
+        CC98APIInterface request = CC98APIManager.createApiClient();
+
+        //对 发送请求 进行封装
+        Observable<ArrayList<HotTopicInfo>> call = request.getTopicHot();
+
+        call.subscribe(new Subscriber<ArrayList<HotTopicInfo>>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onNext(ArrayList<HotTopicInfo> hotTopicInfos) {
+                        for(HotTopicInfo i:hotTopicInfos){
+                            print(i.getTitle());
+                        }
+                        //System.exit(1);
+
+                    }
+                });
+
+        System.out.println();
+
+
+    }
+
+
+    private void print(Object msg){
+        System.out.println(msg);
+    }
+
+
+
+
+
+
+}
